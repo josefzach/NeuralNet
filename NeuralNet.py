4e49ab449d6f99a0fe7b28bb6b7f8a1f916e55f8
Rafael Karrer <rafael.karrer@sonova.com>
2019-04-18 17:40:26 +0200

    Progress on class NeuralNet

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,3 +1,5 @@
+import CostFunctions
+
 class NeuralNet:
     def __init__( self ):
         self.nnLayers = list()
@@ -12,11 +14,20 @@ class NeuralNet:
             
         self.numLayers += 1
 
-    def ForwardProp( self ):
+    def ForwardProp( self, A ):
         A = None
-        for i in range( nlayers ):
-            A = DNN[i].ForwardProp( A )
+        for i in range( self.numLayers ):
+            A = self.nnLayers[i].ForwardProp( A )
 
+    def BackwardProp( self, dA ):
+        for i in range( self.numLayers-1, -1, -1 ):
+            dA = self.nnLayers[i].BackwardProp( dA )  
+
+    def UpdateParams( self, alpha )
+        for i in range( self.numLayers ):
+            #print( i )
+            A = self.nnLayers[i].UpdateParams( alpha )
+        
     def ComputCost( self ):
-        C = -1/np.shape( nnLayers[-1].A )[1] * np.sum( CrossEntropyFcn( Y, A ) )
+        C = -1/np.shape( self.nnLayers[-1].A )[1] * np.sum( CrossEntropyFcn( Y, A ) )
 
